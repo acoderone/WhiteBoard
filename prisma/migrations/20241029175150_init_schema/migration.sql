555c915092a0ca958135801bc49dc0e6415e40ce
Acoderone <avi5200786@gmail.com>
2024-10-29 23:46:09 +0530

    nextauth

--- prisma/migrations/20241029175150_init_schema/migration.sql
+++ prisma/migrations/20241029175150_init_schema/migration.sql
@@ -7,5 +7,15 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Board" (
+    "id" SERIAL NOT NULL,
+    "owner_id" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Board_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
